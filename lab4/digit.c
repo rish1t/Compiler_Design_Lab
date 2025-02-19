d0429aac4d32b148684169ff441e0c5044c7b8ad
Rishit Mandal <118049788+rish1t@users.noreply.github.com>
2025-02-19 20:38:12 +0530

    lab 4
    lab 4 symbol table

---
+++ lab4/digit.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+int main(){
+    int a = 5, b = 6;
+    long arr[10];
+    if(a || b & b){
+        b = b*a;
+    }
+    char str[15] = "Rishit";
+    return a; 
+    //int hopefully don'y see this
+    /*
+    or who knows*/
+}
